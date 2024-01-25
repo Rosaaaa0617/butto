17a9e1303f7077b5ecf9bc9ff55cb257288de097
chen <chenlingxiao0617@163.com>
2024-01-25 19:27:20 +0800

    2dcrush done

---
+++ .vscode/ansa.bat
@@ -0,0 +1,3 @@
+@echo off
+call "C:\Users\ADMIN\AppData\Local\Apps\BETA_CAE_Systems\ansa_v23.0.1\ansa64.bat" -translfp "E:\#code\butto\.vscode\open.py"
+
---
+++ .vscode/open.py
@@ -0,0 +1,4 @@
+from ansa import base,session,constants
+
+session.New("discard")
+base.SetCurrentDeck(constants.LSDYNA)
